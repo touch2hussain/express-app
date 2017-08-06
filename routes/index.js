6aea1a14b8bc54127a8e5e1c432a730cdfab72d0
touch2hussain <touch2hussain@gmail.com>
2017-08-06 14:05:08 +0300

    first comiit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'Express' });
+});
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
---
+++ views/first_view.pug
@@ -0,0 +1,6 @@
+doctype html
+html
+   head
+      title = "Hello Pug"
+   body
+      p.greetings#people Hello World!
